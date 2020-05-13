0eeb020fe98387d03eb7d297de3240e38e021426
Shengyou Fan <shengyoufan@gmail.com>
2020-05-13 17:33:12 +0800

    open route to print Hello, World

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,31 @@
 package cn.kraftsman
 
 import io.ktor.application.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.jackson.jackson
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-}
 
+    install(ContentNegotiation) {
+        jackson {
+
+        }
+    }
+
+    routing {
+
+        get("/") {
+            call.respondText("Hello, world")
+        }
+
+    }
+
+}
